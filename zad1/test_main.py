7a3fc6b2fda64e8819029f200d4c38289b015afe
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-01-25 21:38:09 +0100

    driver update

--- zad1/test_main.py
+++ zad1/test_main.py
@@ -1,11 +1,13 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class MyTestCase(unittest.TestCase):
     def test_title(self):
-        driver = webdriver.Chrome(executable_path='../drivers/chromedriver')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.implicitly_wait(10)
         driver.get("https://duckduckgo.com/")
         driver.find_element(By.ID, "search_form_input_homepage").send_keys("Selenium")
