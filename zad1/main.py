69243eb629c27265168da618eb7ae2e8ee86d853
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-01-25 21:10:09 +0100

    zad1

---
+++ zad1/main.py
@@ -0,0 +1,18 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class MyTestCase(unittest.TestCase):
+    def test_title(self):
+        driver = webdriver.Chrome(executable_path='./chromedriver')
+        driver.implicitly_wait(10)
+        driver.get("https://duckduckgo.com/")
+        driver.find_element(By.ID, "search_form_input_homepage").send_keys("Selenium")
+        driver.find_element(By.ID, "search_button_homepage").submit()
+        self.assertEqual(driver.title, "Selenium at DuckDuckGo")
+        driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
